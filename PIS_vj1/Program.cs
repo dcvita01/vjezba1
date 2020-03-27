0d558b4a011a474d6524e90b1e176ce87f4cbb39
Tomislav Petrov <45316206+TomislavP98@users.noreply.github.com>
2020-03-27 16:04:12 +0100

    Removing commented output
    Uncommented part of code that writes out "Hello World!" on console

--- PIS_vj1/Program.cs
+++ PIS_vj1/Program.cs
@@ -6,7 +6,7 @@ namespace PIS_vj1
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
